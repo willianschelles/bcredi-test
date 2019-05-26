5fab1197a57cc3563f2873e6b76b44c44847808d
willian.schelles <willian@musca.io>
2019-05-26 19:42:48 -0300

    Adding client e server directories; starting server; receiving request from client; making requesting to the github API, and retorning and displaying a 'raw' json

---
+++ client/src/App.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+
+  state = {
+    response: '',
+    post: '',
+    responseToPost: ''
+  };
+
+  componentDidMount() {
+
+    this.callApi()
+      .then(res => this.setState({ response: res.express }))
+      .catch(err => console.error(err));
+  }
+
+  callApi = async() => {
+    const response = await fetch('/api/search/highlights-repositories');
+    const body = await response.json();
+    console.log(body)
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
+  };
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch('/api/world', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ post: this.state.post }),
+    });
+
+    const body = await response.text();
+    this.setState({ responseToPost: body });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Qual Senóide? Willian the master
+          </p>
+          <a
+            className="Search Repositories"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <p>{this.state.response}</p>
+        <form onSubmit={this.handleSubmit}>
+          <p>
+            <strong>Post to Server:</strong>
+          </p>
+          <input
+            type="text"
+            value={this.state.post}
+            onChange={e => this.setState({ post: e.target.value })}
+          />
+          <button type="submit">Submit</button>
+        </form>
+        <p>{this.state.responseToPost}</p>
+      </div>
+    );
+  }
+};
+
+export default App;
---
+++ server/communication/github-request.js
@@ -0,0 +1,75 @@
+const https = require('https');
+
+
+class GithubRequest {
+
+    constructor (languages) {
+        this.languages = languages
+    }
+    
+    buildOptions (language) {
+        return  {
+            host: 'api.github.com',
+            path: `/search/repositories?o=desc&q=${language}&s=stars&type=Repositories&page=1&per_page=1`,
+            port: 443,
+            method: 'GET',
+            headers: {
+                'user-agent':'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)',
+                'Authorization': 'Bearer 7c7e72e34ae97ede72455d79f06c060c3e64298e'
+            }
+        };
+    }
+
+    async getFiveMajorReposFor(language) {
+        return new Promise((resolve, reject) => {
+            const options = this.buildOptions(language);
+    
+            const req = https.request(options, (response) => {
+                let data = '';
+                response.on('data', (chunk) => {
+                    data += chunk;
+                    // process.stdout.write(data);
+                });
+        
+                response.on('end', () => {
+        
+                    const repos = JSON.parse(data)['items'];
+                    console.log('repos.length')
+                    console.log(repos)
+                    console.log(repos.length)
+                    return resolve(repos);
+                    // array.forEach(element => {
+                    //     console.log(element)
+                    //     console.log()
+                    // });
+                });
+            })
+        
+            req.on('erro', (err) => {
+                console.error(err);
+            });
+        
+            req.end();
+        })
+    };
+    
+    // const bangDosBanguers = `o=desc&q=elixir&s=stars&type=Repositories&page=1&per_page=5`;
+
+    async makeRequest () {
+        return new Promise((resolve, reject) => {
+
+            const response = [];
+            this.languages.forEach(async (language) => {
+                const fiveRepo = await this.getFiveMajorReposFor(language);
+                response.push(fiveRepo);
+
+                if (response.length === this.languages.length) {
+                    return resolve({response});
+                }
+            });
+        })
+    }
+}
+
+module.exports.GithubRequest = GithubRequest;
+    
