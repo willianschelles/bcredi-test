a1d46f13f6f8b1b10a000c087aa38b8adcacc6f0
willian.schelles <willian@musca.io>
2019-05-29 18:01:29 -0300

    Fixing

--- server/index.js
+++ server/index.js
@@ -9,6 +9,10 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get("/*", function (req, res) {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+})
+
 const languages = ['elixir', 'ruby', 'python', 'java', 'JavaScript'];
 
 class StartApp {
