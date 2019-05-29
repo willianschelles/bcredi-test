51ed19e024b8916af97bfeaa44a96fd1cb5d9eac
willian.schelles <willian@musca.io>
2019-05-29 14:25:04 -0300

    Chnging db client options to put heroku postgres addon config

--- server/model/database.js
+++ server/model/database.js
@@ -8,10 +8,10 @@ class Database {
 
     getClient() {
         return new Client({
-            user: 'willian',
-            host: 'localhost',
-            database: 'bcredi',
-            password: 'bcredi',
+            user: 'luirxvoivkkldr',
+            host: process.env.DATABASE_URL || 'ec2-54-197-232-203.compute-1.amazonaws.com';
+            database: 'd2ikmlaibcnj3d',
+            password: 'e743de633ab544d686995966c596d8bed86112cbd8ef65cd58426647b53626f4',
             port: 5432,
         })
     }
